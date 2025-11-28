bc5807da80e09730ee887773a956dbedc2a32374
Vitek-1 <vitfrolik@gmail.com>
2025-11-28 10:07:15 +0100

    example in Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        Listik listik = new Listik();
+        listik.add(3);
+        listik.add(7);
+        listik.add(53);
+        listik.add(1);
+        listik.add(64);
+        listik.add(90);
+
+        IteratorListik iterator = (IteratorListik) listik.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }
