44378edcb87c10614e3527c7ddf1f4b9ed1d15a4
Vitek-1 <vitfrolik@gmail.com>
2025-11-28 09:51:51 +0100

    create new class IteratorListik with constructor

--- src/IteratorListik.java
+++ src/IteratorListik.java
@@ -1,2 +1,23 @@
-public class IteratorListik {
+import java.util.Iterator;
+
+public class IteratorListik implements Iterator<Integer> {
+
+    private Listik listik;
+    private int aktualniIndex;
+
+    public IteratorListik(Listik listik) {
+        this.listik = listik;
+        this.aktualniIndex = listik.size() -1;
+    }
+
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        return 0;
+    }
 }
